c68134634508fa129f4458e6bdd66085bc891e5a
Aditya Bintang F <45384894+adityabintang@users.noreply.github.com>
2021-06-01 11:41:16 +0700

    Membuat modularisasi pada aplikasi dan membuat fungsi delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'quote.dart';
-
+import 'quote_card.dart';
 void main() {
   runApp(MaterialApp(
     home: NinjaCard(),
@@ -188,38 +188,9 @@ class _QuoteState extends State<Quote> {
         text: 'The truth is rarely pure and never simple')
   ];
 
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
-            ),
-            SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
-            ),
-            SizedBox(height: 8.0),
-            FlatButton.icon(
-              onPressed: () {
-                setState(() {
-                  quotes.remove(quote);
-                });
-              },
-              label: Text("delete quote"),
-              icon: Icon(Icons.delete),
-            )
-          ],
-        ),
-      ),
-    );
-  }
+  // Widget quoteTemplate(quote) {
+  //   return QuoteCard(quote: quote);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -231,8 +202,17 @@ class _QuoteState extends State<Quote> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        children: quotes.map((quote) => QuoteCard(
+            quote: quote,
+            delete: () {
+              setState(() {
+                quotes.remove(quote);
+              });
+            }
+        )).toList(),
       ),
     );
   }
 }
+
+
